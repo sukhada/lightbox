b0ee7d56ffd627f0e23ae5eecc20cea30554a05e
Sukhada Kulkarni <skulkarni@hearsaycorp.com>
2017-05-07 10:54:10 -0700

    Added prev, next & next images preview

--- slack.js
+++ slack.js
@@ -62,6 +62,33 @@ function renderPhotos(responseJSON) {
 	var photos = responseJSON.photos.photo;
 	this.lightbox = new Lightbox();
 	var self = this;
+
+	document.onkeydown = function(evt) {
+	    evt = evt || window.event;
+	    var isEscape = false;
+	    var isNext = false;
+	    var isPrev = false;
+
+	    if ("key" in evt) {
+	        isEscape = (evt.key == "Escape" || evt.key == "Esc");
+	        isNext = (evt.key == "ArrowRight");
+	        isPrev = (evt.key == "ArrowLeft");
+	    } else {
+	        isEscape = (evt.keyCode === 27);
+	        isNext = (evt.keyCode === 39);
+	        isPrev = (evt.keyCode === 37);
+	    }
+	    if (isEscape) {
+	    	self.lightbox.closeModal();
+	    }
+	    if (isNext) {
+	    	self.lightbox.renderSlide(self.lightbox.currIndex+1)
+	    }
+	    if (isPrev) {
+	    	self.lightbox.renderSlide(self.lightbox.currIndex-1)
+	    }
+	};
+	
 	for (var i = 0; i < len-1; i++) {
 		var photo = photos[i];
 		this.flickrService.getPhotoInfo(photo.id, photo.secret, this.lightbox.appendPhoto, i);
@@ -73,16 +100,35 @@ function renderPhotos(responseJSON) {
 		self.lightbox.closeModal();
 	});
 	prev.addEventListener('click', function(event) {
-		self.lightbox.renderSlide()
+		self.lightbox.renderSlide(self.lightbox.currIndex-1)
 	});
+	next.addEventListener('click', function(event) {
+		self.lightbox.renderSlide(self.lightbox.currIndex+1)
+	});	
+	var nextImageNodes = document.getElementsByClassName('nextImgNode');
+	for (var i = 0; i < nextImageNodes.length; i++) {
+		var nextImageNode = nextImageNodes[i];
+		nextImageNode.addEventListener('click', function(event) {
+			if (event.srcElement && event.srcElement.src) {
+				var index = event.srcElement.getAttribute('data-image-num');
+				console.log(index);
+				self.lightbox.renderSlide(index);			
+			}				
+		});
+	}	
 }
 
-function Photo(url, title) {
-	this.url = url;
+function Photo(mediumURL, highResURL, title) {
+	this.mediumURL = mediumURL;
+	this.highResURL = highResURL;
 	this.title = title;
 
-	this.getURL = function() {
-		return this.url;
+	this.getHighResURL = function() {
+		return this.highResURL;
+	}
+
+	this.getMediumSizeURL = function() {
+		return this.mediumURL;
 	}
 
 	this.getTitle = function() {
@@ -91,17 +137,18 @@ function Photo(url, title) {
 }
 
 function constructPhoto(jsonResponse) {
-	var url = constructImageURL(jsonResponse);
+	var mediumURL = constructImageURL(jsonResponse, 'z');
+	var highResURL = constructImageURL(jsonResponse, 'h');
 	var title = jsonResponse.title._content;	
-	return new Photo(url, title);
+	return new Photo(mediumURL, highResURL, title);
 }
 
-function constructImageURL(photoJSON) {
+function constructImageURL(photoJSON, size) {
 	var farm = photoJSON.farm;
 	var server = photoJSON.server;
 	var id = photoJSON.id;
 	var secret = photoJSON.secret;
-	return 'https://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '.jpg'
+	return 'https://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '_' + size + '.jpg'
 }
 
 function constructTitle(photoJSON) {
@@ -123,16 +170,15 @@ function Lightbox() {
 		var ul = document.getElementsByClassName('photo-list')[0];
 		var li = document.createElement('li');
 		var img = document.createElement('img');
-		img.src = photoObj.getURL();
+		img.src = photoObj.getMediumSizeURL();
 		img.title = photoObj.getTitle();
-		img.setAttribute('data-image-num', index)
+		img.setAttribute('data-image-num', self.photos.length - 1)
 		li.append(img);
 		ul.append(li);
 		li.addEventListener('click', function(event) {
 			event.preventDefault();
 			if (event.srcElement && event.srcElement.src) {
 				var index = event.srcElement.getAttribute('data-image-num');
-				console.log(index);
 				self.openModal();
 				self.renderSlide(index);			
 			}
@@ -151,15 +197,28 @@ function Lightbox() {
 
 	this.renderSlide = function(num) {
 		var photo = this.photos[num];
-		var imgURL = photo.getURL();
+		var imgURL = photo.getHighResURL();
 		var imgTitle = photo.getTitle();
 		var currentImage = document.getElementsByClassName('currImage')[0];
 		var currentImageNode = document.getElementsByClassName('currImageNode')[0];
 		var currentTitle = document.getElementsByClassName('currTitle')[0];
 		currentTitle.textContent = imgTitle;
-		currentImageNode.src = imgURL;	
+		currentImageNode.src = imgURL;
+		this.currIndex = parseInt(num,10);
+		var nextImages = document.getElementsByClassName('nextImages')[0];
+		var nextPhotos = this.getNextPhotos(parseInt(num,10));
+	}
+
+	this.getNextPhotos = function(num) {
+		for (var i = 1; i < 5; i++) {
+			var nextPhoto = this.photos[num+i];
+			var nextImageNode = document.getElementsByClassName('nextImgNode' + i)[0];
+			nextImageNode.src = nextPhoto.getMediumSizeURL();
+			nextImageNode.setAttribute('data-image-num', num+i);
+		}
 	}
 }
+	
 
 this.flickrService = new FlickrPhotoService();
 this.flickrService.getPhotos();
